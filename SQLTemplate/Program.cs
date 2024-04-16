2e992ce65f88667d0a311073308c220d14c96be9
Al Stone <astone@heckgrammar.co.uk>
2024-04-16 09:40:20 +0100

    Add project files.

---
+++ SQLTemplate/Program.cs
@@ -0,0 +1,27 @@
+﻿using MySqlConnector;
+namespace SQLTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string server,username,password,database;
+            Console.Write("Enter server IP: ");
+            server = Console.ReadLine();
+            Console.Write("Enter username: ");
+            username = Console.ReadLine();
+            Console.Write("Enter password: ");
+            password = Console.ReadLine();
+            Console.Write("Enter database: ");
+            database = Console.ReadLine();
+            MySqlConnection conn = con(server,username,password,database);
+            
+        }
+        static MySqlConnection con(string server,string user, string password, string database)
+        {
+            string connectDetails = $"server={server};uid={user};password={password};database={database}";
+            MySqlConnection con = new MySqlConnection(connectDetails);
+            return con;
+        }
+    }
+}
